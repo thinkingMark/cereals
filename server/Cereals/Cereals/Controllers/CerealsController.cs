044e04a288e687b91027c9ec6848db5628c517bb
DESKTOP-O93NATE\markr <mark.rakochyi@gmail.com>
2021-01-26 23:54:43 +0200

    Revert "fixes"
    This reverts commit 2ff6d05781aebc251df815a8ac7991231dbcb900.

---
+++ server/Cereals/Cereals/Controllers/CerealsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cereals.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cereals.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CerealsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public CerealsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getProducts")]
+        public async Task<IActionResult> GetData(string request)
+        {
+            try
+            {
+                var data = _productService.GetData(request);
+                return Ok(data.Result);
+            }
+            catch(Exception e)
+            {
+                return StatusCode(500, e);
+            }
+        }
+    }
+}
